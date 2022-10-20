9a8e9fa3cd8a35682b1fb4abe5aef582f0c28324
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2022-10-20 19:37:02 +0300

    Project

---
+++ Accounts/Accounts.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+struct acount {
+    int number;
+    std::string name;
+    float balance;
+};
+
+void change_acount(acount& random) {
+    int x = 0;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> x;
+
+    random.balance = x;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int number1 = 0;
+    std::string name1;
+    float balance1;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> number1;
+
+    std::cout << "Введите имя владельца: ";
+    std::cin >> name1;
+
+    std::cout << "Введите баланс: ";
+    std::cin >> balance1;
+
+    acount random = {number1, name1, balance1};
+    change_acount(random);
+    std::cout << "Изменённый баланс: " << random.balance << std::endl;
+
+    std::cout << "Ваш счёт: " << random.name << ", " << random.number << ", " << random.balance;
+}
---
+++ StructureOutput/StructureOutput.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+struct Address {
+    std::string county;
+    std::string city;
+    std::string street;
+    int house;
+    int Flat;
+    int index;
+};
+
+void func(Address& A, std::string country, std::string city, std::string street, int house, int Flat, int index) {
+    
+    std::cout << "Страна: " << country << std::endl;
+    std::cout << "Город: " << city << std::endl;
+    std::cout << "Улица: " << street << std::endl;
+    std::cout << "Дом: " << house << std::endl;
+    std::cout << "Квартира: " << Flat << std::endl;
+    std::cout << "Индекс: " << index << std::endl;
+
+    std::cout << A.county << " " << A.city;
+    
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    std::string country;
+    std::string city;
+    std::string street;
+    int house;
+    int Flat;
+    int index;
+
+
+    std::cout << "Страна: ";
+    std::cin >> country;
+
+    std::cout << "Город: ";
+    std::cin >> city;
+
+    std::cout << "Улица: ";
+    std::cin >> street;
+
+    std::cout << "Дом: ";
+    std::cin >> house;
+
+    std::cout << "Квартира: ";
+    std::cin >> Flat;
+
+    std::cout << "Индекс: ";
+    std::cin >> index;
+
+    std::cout << "\n" << std::endl;
+
+    Address r;
+    r.county = "Россия";
+    r.city = "Ижевск";
+    r.street = "Пушкина";
+    r.house = 59;
+    r.Flat = 143;
+    r.index = 983769;
+
+
+    std::cout << "Страна: " << r.county << "\n" << "Город: " << r.city << "\n" << "Улица" << r.street << "\n"
+        << "Дом: " << r.house << "\n" << "Квартира: " << r.Flat << "\n" << "Индекс: " << r.index;
+}
