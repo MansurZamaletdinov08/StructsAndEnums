188d38a582d083e7855815bb36cb6a1c23dbe966
Mansur Zamaletdinov <101476332+MansurZamaletdinov08@users.noreply.github.com>
2022-10-22 10:23:46 +0300

    Update StructureOutput.cpp

--- StructureOutput/StructureOutput.cpp
+++ StructureOutput/StructureOutput.cpp
@@ -1,7 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 
 struct Address {
-    std::string county;
+    std::string country;
     std::string city;
     std::string street;
     int house;
@@ -9,18 +9,17 @@ struct Address {
     int index;
 };
 
+
 void func(Address& A, std::string country, std::string city, std::string street, int house, int Flat, int index) {
-    
+
     std::cout << "Страна: " << country << std::endl;
     std::cout << "Город: " << city << std::endl;
     std::cout << "Улица: " << street << std::endl;
     std::cout << "Дом: " << house << std::endl;
     std::cout << "Квартира: " << Flat << std::endl;
     std::cout << "Индекс: " << index << std::endl;
-
-    std::cout << A.county << " " << A.city;
-    
 }
+
 
 int main() {
     setlocale(LC_ALL, "Ru");
@@ -49,18 +48,10 @@ int main() {
 
     std::cout << "Индекс: ";
     std::cin >> index;
+    std::cout << std::endl;
 
-    std::cout << "\n" << std::endl;
 
     Address r;
-    r.county = "Россия";
-    r.city = "Ижевск";
-    r.street = "Пушкина";
-    r.house = 59;
-    r.Flat = 143;
-    r.index = 983769;
-
-
-    std::cout << "Страна: " << r.county << "\n" << "Город: " << r.city << "\n" << "Улица" << r.street << "\n"
-        << "Дом: " << r.house << "\n" << "Квартира: " << r.Flat << "\n" << "Индекс: " << r.index;
+    func(r, country, city, street, house, Flat, index);
+    
 }
