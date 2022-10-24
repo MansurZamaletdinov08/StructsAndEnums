3fec4de555280ada6318c8c6c6a0baa8aab1f322
Mansur Zamaletdinov <101476332+MansurZamaletdinov08@users.noreply.github.com>
2022-10-24 20:38:45 +0300

    Update StructureOutput.cpp

--- StructureOutput/StructureOutput.cpp
+++ StructureOutput/StructureOutput.cpp
@@ -10,48 +10,62 @@ struct Address {
 };
 
 
-void func(Address& A, std::string country, std::string city, std::string street, int house, int Flat, int index) {
-
-    std::cout << "Страна: " << country << std::endl;
-    std::cout << "Город: " << city << std::endl;
-    std::cout << "Улица: " << street << std::endl;
-    std::cout << "Дом: " << house << std::endl;
-    std::cout << "Квартира: " << Flat << std::endl;
-    std::cout << "Индекс: " << index << std::endl;
+void func(Address& A) {
+    std::cout << "Страна: " << A.country << std::endl;
+    std::cout << "Город: " << A.city << std::endl;
+    std::cout << "Улица: " << A.street << std::endl;
+    std::cout << "Дом: " << A.house << std::endl;
+    std::cout << "Квартира: " << A.Flat << std::endl;
+    std::cout << "Индекс: " << A.index << std::endl;
 }
 
 
 int main() {
     setlocale(LC_ALL, "Ru");
-    std::string country;
-    std::string city;
-    std::string street;
-    int house;
-    int Flat;
-    int index;
-
-
+    Address v1;
+    std::cout << "Первая структура: " << std::endl;
     std::cout << "Страна: ";
-    std::cin >> country;
+    std::cin >> v1.country;
 
     std::cout << "Город: ";
-    std::cin >> city;
+    std::cin >> v1.city;
 
     std::cout << "Улица: ";
-    std::cin >> street;
+    std::cin >> v1.street;
 
-    std::cout << "Дом: ";
-    std::cin >> house;
+    std::cout << "Дом:";
+    std::cin >> v1.house;
 
     std::cout << "Квартира: ";
-    std::cin >> Flat;
+    std::cin >> v1.Flat;
 
     std::cout << "Индекс: ";
-    std::cin >> index;
-    std::cout << std::endl;
+    std::cin >> v1.index;
+    std::cout << "\n\n";
+    std::cout << "Вывод функции: " << std::endl;
+    func(v1);
 
+    Address v2;
+    std::cout << "\nВторая структура: \n" << std::endl;
+    std::cout << "Страна: ";
+    std::cin >> v2.country;
 
-    Address r;
-    func(r, country, city, street, house, Flat, index);
-    
+    std::cout << "Город: ";
+    std::cin >> v2.city;
+
+    std::cout << "Улица: ";
+    std::cin >> v2.street;
+
+    std::cout << "Дом:";
+    std::cin >> v2.house;
+
+    std::cout << "Квартира: ";
+    std::cin >> v2.Flat;
+
+    std::cout << "Индекс: ";
+    std::cin >> v2.index;
+    std::cout << "\n\n";
+    std::cout << "Вывод второй функции: ";
+
+    func(v2);
 }
